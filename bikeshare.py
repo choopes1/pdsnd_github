68c233f2e8ae44396256ed5cd1f8fd74305adfe8
Craig Hoopes <craighoopes1@gmail.com>
2018-11-06 19:04:05 -0500

    Removed 'TO DO' from all comments in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -24,7 +24,7 @@ def get_filters():
 
     print("\nHello! Let's explore some US bikeshare data!")
 
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # Get user input for city (chicago, new york city, washington).
     while True:
         city = input("\nWould you like to see data for Chicago, New York City, or Washington?\n")
         city = city.lower()
@@ -43,7 +43,7 @@ def get_filters():
         else:
             break
 
-    # TO DO: get user input for month (january, february, ... , june)
+    # Get user input for month (january, february, ... , june)
     if time_filter == "month":
         while True:
             month = input("\nWhich month?  January, February, March, April, May, or June?\n")
@@ -54,7 +54,7 @@ def get_filters():
             else:
                 break
 
-    # TO DO: get user input for day of week (monday, tuesday, ... sunday)
+    # Get user input for day of week (monday, tuesday, ... sunday)
     if time_filter == "day":
         while True:
             day = input("\nWhich day?  Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday?\n")
@@ -83,25 +83,25 @@ def load_data(city, month, day):
 # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
 
-    # convert the Start Time column to datetime
+    # Convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
-    # extract month and day of week from Start Time to create new columns
+    # Extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
 
-    # filter by month if applicable
+    # Filter by month if applicable
     if month != 'all':
-        # use the index of the months list to get the corresponding int
+        # Use the index of the months list to get the corresponding int
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = int(months.index(month) + 1)
 
-        # filter by month to create the new dataframe
+        # Filter by month to create the new dataframe
         df = df[df['month'] == month]
 
-    # filter by day of week if applicable
+    # Filter by day of week if applicable
     if day != 'all':
-        # filter by day of week to create the new dataframe
+        # Filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
 
     return df
@@ -112,16 +112,16 @@ def time_stats(df):
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-    # TO DO: display the most common month
+    # Display the most common month
     months = ['January', 'February', 'March', 'April', 'May', 'June']
     popular_month = months[df['month'].mode()[0] - 1]
     print("Most popular month for traveling: ", popular_month)
 
-    # TO DO: display the most common day of week
+    # Display the most common day of week
     popular_day = df['day_of_week'].mode()[0]
     print("Most popular day for traveling: ", popular_day)
 
-    # TO DO: display the most common start hour
+    # Display the most common start hour
     df['hour'] = df['Start Time'].dt.hour
     popular_start_hour = df['hour'].mode()[0]
     print("Most popular hour of the day to start traveling: ", popular_start_hour)
@@ -136,15 +136,15 @@ def station_stats(df):
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
-    # TO DO: display most commonly used start station
+    # Display most commonly used start station
     popular_start_station = df['Start Station'].mode()[0]
     print("Most popular start station: ", popular_start_station)
 
-    # TO DO: display most commonly used end station
+    # Display most commonly used end station
     popular_end_station = df['End Station'].mode()[0]
     print("Most popular end station: ", popular_end_station)
 
-    # TO DO: display most frequent combination of start station and end station trip
+    # Display most frequent combination of start station and end station trip
     popular_trip = (df["Start Station"] + " to " + df["End Station"]).mode()[0]
     print("Most popular trip from start to end: ", popular_trip)
 
@@ -158,12 +158,12 @@ def trip_duration_stats(df):
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
-    # TO DO: display total travel time
+    # Display total travel time
     total_seconds = df['Trip Duration'].sum()
     convert_total_seconds = timedelta(seconds=int(total_seconds))
     print("The total travel time is: {} seconds ({})".format(total_seconds, convert_total_seconds))
 
-    # TO DO: display mean travel time
+    # Display mean travel time
     mean_seconds = df['Trip Duration'].mean()
     convert_mean_seconds = timedelta(seconds=int(mean_seconds))
     print("The average travel time is: {} seconds ({})".format(mean_seconds, convert_mean_seconds))
@@ -178,12 +178,12 @@ def user_stats(df):
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
-    # TO DO: Display counts of user types
+    # Display counts of user types
     user_types = df['User Type'].value_counts()
     print("\nNumber of users by type:\n")
     print(user_types)
 
-    # TO DO: Display counts of gender
+    # Display counts of gender
     if 'Gender' in df.columns:
         gender = df['Gender'].value_counts()
         print("\nNumber of users by gender:\n")
@@ -191,7 +191,7 @@ def user_stats(df):
     else:
         print("\nNo gender data available.\n")
 
-    # TO DO: Display earliest, most recent, and most common year of birth
+    # Display earliest, most recent, and most common year of birth
     if 'Birth Year' in df.columns:
         earliest_year = int(df['Birth Year'].describe()["min"])
         latest_year = int(df['Birth Year'].describe()["max"])
